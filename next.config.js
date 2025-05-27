db6ca7ea426136450560a708cc6076db5be5a179
Manasa Kalaimalai <manasak@macs-MacBook-Pro.local>
2025-05-27 23:33:35 +0530

    Update Next.js configuration for Vercel deployment

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['localhost'],
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = nextConfig;
